d0e853bfc0367b1ece8c796c229cccc6025cfb18
tonible14012002 <namanh14.work@gmail.com>
2023-11-25 18:38:50 +0700

    feat: create backup scripts for database

--- core_apps/stories/urls.py
+++ core_apps/stories/urls.py
@@ -3,9 +3,9 @@ from rest_framework.routers import SimpleRouter
 from . import views
 
 stories_router = SimpleRouter()
-stories_router.register('archieved', views.UserAchievedStory, 'archieved')
-stories_router.register('following', views.FollowingStory, 'following')
-stories_router.register('all', views.Story, 'story')
+stories_router.register('archieved', views.UserAchievedStory, 'archieved')  # expired
+stories_router.register('following', views.FollowingStory, 'following')  # Expired
+stories_router.register('all', views.Story, 'story')  # Test only
 
 urlpatterns = [
     path('', include(stories_router.urls)),
